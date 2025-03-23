d9ccff9d8fa60faa0dfcd9ad0de5d337ab6acae5
Rajat Chowdhury <91901355+deathnote995@users.noreply.github.com>
2025-03-23 17:23:38 +0000

    conatiner config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const constantVariable=123456
+let letvariable="salalSingh"
+var varVariable="salalSinghGober"
+keywordLessVariable="salal Singh"
+let undefinedVariable
+
+//constantVariable=9876    cant be changed 
+
+console.log(letvariable);
+console.table([constantVariable,letvariable,varVariable,keywordLessVariable,undefinedVariable]);
+
+
+
+/*
+    dont use var beause it is outdated dur its block scoping and functional scoping problem
+    also dont use keyword less variable, it is a bad practice 
+ */
