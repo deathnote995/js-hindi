e8a972f1c76ba5e03da476eb83c8a992a6bd7a91
Rajat Chowdhury <91901355+deathnote995@users.noreply.github.com>
2025-04-11 10:39:47 +0000

    date done

---
+++ 01_basics/Date_Time.js
@@ -0,0 +1,10 @@
+const myDate=Date.now();
+console.log(myDate);
+
+const myDate2=myDate.toLocaleString();
+console.log(myDate2);
+
+const myDate3=new Date();
+console.log(myDate3);
+console.log(myDate3.toISOString());
+console.log(myDate3.getHours());
